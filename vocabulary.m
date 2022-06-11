8f8cbdee897dac0f20704812c9ed5337327c2a3d
ACherkas12 <55573379+ACherkas12@users.noreply.github.com>
2022-06-11 20:27:39 +0300

    Create vocabulary.m

---
+++ vocabulary.m
@@ -0,0 +1,58 @@
+## A
+
+## B
+
+## C
+churlish = rude
+
+## D
+
+## E
+
+## F
+facetoius = treating a serious topic in an inappropriately humorous way
+flimsy (флимзи) =  ненадежный (weak and easily to destroyed) (+ argument, excuse)
+
+## G
+
+## H
+
+## I
+
+## J
+
+## K
+
+## L
+
+## M
+
+## N
+
+## O
+
+## P
+petty = unimportant (petty issue like this)
+pertinent/impertinent = suitable/insuitable (theme)
+
+## Q
+
+## R
+
+## S
+
+## T
+
+## U
+unacceptable = неприемлемо
+
+## V
+
+## W
+wearisome = boring
+
+## X
+
+## Y
+
+## Z
